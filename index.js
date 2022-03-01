dd24a0bdbb356b8305374d0869f4eae3960898ce
shanaz <56303132+chrisshanaz@users.noreply.github.com>
2022-03-01 18:38:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,19 +18,28 @@ client.on("ready", function() {
   console.log("READY FOR ACTION!");
 });
 
-var isBenOn = new Boolean(false);
-var gifMode = new Boolean(false);
+var isBenOn = {};
+var gifMode = {};
+var isReading = {};
+
 let RNG = 0;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-isBenOn = false;
-gifMode = false;
-
 client.on('message', message => {
 
+  if(typeof isBenOn === 'undefined'){
+     isBenOn[message.guild.id] = new Boolean(false); 
+  }
+  if(typeof gifMode === 'undefined'){
+     gifMode[message.guild.id] = new Boolean(false); 
+  }
+  if(typeof isReading === 'undefined'){
+     isReading[message.guild.id] = new Boolean(false); 
+  }
+  
   if(message.content.toLowerCase().includes("ben help")){
     const exampleEmbed = new MessageEmbed()
 	  .setColor('#0099ff')
@@ -41,7 +50,7 @@ client.on('message', message => {
 	  	{ name: 'How To Activate', value: 'If I hang up or am not started, just ping me or write "ben start"' },
   		{ name: 'How to chat with me', value: 'Simply say ben, Ben, or @ me to get a response', inline: true },
   		{ name: 'Special Commands', value: 'ben embed fail. ben dover. ben chug. ben eat. ben gifmode', inline: true },
-      { name: 'Bot Version', value: 'v1.1, bot by shanaz#6969'},
+      { name: 'Bot Version', value: 'v1.12, bot by shanaz#6969'},
   	)  
 
     message.channel.send({ embeds: [exampleEmbed] });
@@ -49,8 +58,8 @@ client.on('message', message => {
   }
 
   if(message.content.toLowerCase().includes("ben gifmode")){
-      gifMode = !gifMode;
-      if(gifMode){
+      gifMode[message.guild.id] = !gifMode[message.guild.id];
+      if(gifMode[message.guild.id]){
         message.channel.send("GIF Mode set to **ON**")
       } else {
         message.channel.send("GIF Mode set to **OFF**")
@@ -58,22 +67,21 @@ client.on('message', message => {
       return;
     }
   
-  if((message.mentions.has(client.user) || message.content.toLowerCase().includes("ben start")) && !isBenOn){
-     if(gifMode){
+  if((message.mentions.has(client.user) || message.content.toLowerCase().includes("ben start")) && !isBenOn[message.guild.id]){
+     if(gifMode[message.guild.id]){
        message.channel.send("https://ibb.co/DKNbyGh");
      } else {
        message.channel.send("Ben.");
      }
      
-     isBenOn = true;
+     isBenOn[message.guild.id] = true;
      return;
   }
   
-  if((message.mentions.has(client.user) || message.content.toLowerCase().includes("ben"))&& isBenOn){
+  if((message.mentions.has(client.user) || message.content.toLowerCase().includes("ben"))&& isBenOn[message.guild.id]){
     if(message.author.bot) return;
     
-    RNG = getRandomInt(17);
-    console.log(RNG);
+    RNG = getRandomInt(18);
     
     if(message.content.toLowerCase().includes("dover")){
       message.channel.send("https://tenor.com/view/bend-over-gif-21069300");    
@@ -95,8 +103,44 @@ client.on('message', message => {
       return;
     }
 
+    if(isReading[message.guild.id]){
+      if(RNG === 0 || RNG === 2 || RNG === 3 || RNG === 1 || RNG === 4){
+        if(!gifMode[message.guild.id]){
+          message.channel.send("Na Na Na Na.");
+        } else {
+          message.channel.send("https://ibb.co/YfYJS56");
+        } 
+
+        return;
+      }
+
+      if(RNG === 6 || RNG === 7 || RNG === 8 || RNG === 9 || RNG === 5){
+        if(!gifMode[message.guild.id]){
+          message.channel.send("Shhhhhhhhhhh.");
+        } else {
+          message.channel.send("https://ibb.co/0GbJJRQ");
+        } 
+
+        return;
+      }
+
+      if(RNG === 11 || RNG === 12 || RNG === 13 || RNG === 14 || RNG === 10){
+        if(!gifMode[message.guild.id]){
+          message.channel.send("**snores**.");
+        } else {
+          message.channel.send("https://ibb.co/0GbJJRQ");
+        } 
+
+        return;
+      }
+
+      message.channel.send("https://ibb.co/DKNbyGh");
+      isReading[message.guild.id] = false;
+      return;
+    }
+
     if(RNG === 0 || RNG === 2 || RNG === 3 || RNG === 1){
-      if(!gifMode){
+      if(!gifMode[message.guild.id]){
         message.channel.send("Yes.");
       } else {
         message.channel.send("https://ibb.co/q9ZQTWS");
@@ -104,7 +148,7 @@ client.on('message', message => {
     }
 
     if(RNG === 5 || RNG === 6 || RNG === 7 || RNG === 4){
-      if(!gifMode){
+      if(!gifMode[message.guild.id]){
         message.channel.send("No.");
       } else {
         message.channel.send("https://ibb.co/kyCPwxg");
@@ -112,7 +156,7 @@ client.on('message', message => {
     }
 
     if(RNG === 9 || RNG === 10 || RNG === 11 || RNG === 8){
-      if(!gifMode){
+      if(!gifMode[message.guild.id]){
         message.channel.send("Ho Ho Ho.");
       } else {
         message.channel.send("https://ibb.co/HzQZ3Qb");
@@ -120,7 +164,7 @@ client.on('message', message => {
     }
 
     if(RNG === 13 || RNG === 14 || RNG === 15 || RNG === 12){
-      if(!gifMode){
+      if(!gifMode[message.guild.id]){
         message.channel.send("Ugh.");
       } else {
         message.channel.send("https://ibb.co/x8v8xHB");
@@ -128,12 +172,21 @@ client.on('message', message => {
     }
 
     if(RNG === 16 ){
-      if(gifMode){
+      if(gifMode[message.guild.id]){
         message.channel.send("https://tenor.com/view/talking-ben-ben-hang-up-bye-goodbye-gif-24987183");
       } else {
         message.channel.send(":telephone:");
       }
-      isBenOn = false;
+      isBenOn[message.guild.id]  = false;
+    }
+
+    if(RNG === 17){
+      if(gifMode[message.guild.id]){
+        
+      } else {
+        message.channel.send(":newspaper:");
+      }
+      isReading[message.guild.id] = true; 
     }
   }
 })
